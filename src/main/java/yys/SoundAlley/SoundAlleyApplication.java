32003bc2dbf3c66035ac6fcecead17e4356b2716
noonbbara <ysys1226@naver.com>
2025-05-16 00:21:02 +0900

    [INITIAL]

---
+++ src/main/java/yys/SoundAlley/SoundAlleyApplication.java
@@ -0,0 +1,13 @@
+package yys.SoundAlley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundAlleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundAlleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yys/SoundAlley/SoundAlleyApplicationTests.java
@@ -0,0 +1,13 @@
+package yys.SoundAlley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundAlleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
